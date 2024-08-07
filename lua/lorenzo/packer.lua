4ba4e93c2f1cfa911000d0067f16036ddf9015c2
Lor3nzoMartinez <Lorenzo.s.martinez0@gmail.com>
2024-08-07 10:44:00 -0700

    Add undo tree

--- lua/lorenzo/packer.lua
+++ lua/lorenzo/packer.lua
@@ -20,6 +20,7 @@ return require('packer').startup(function(use)
   use ('vim-airline/vim-airline')
   use ('vim-airline/vim-airline-themes')
 
+  use ('mbbill/undotree')
   use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use ('theprimeagen/harpoon')
 
