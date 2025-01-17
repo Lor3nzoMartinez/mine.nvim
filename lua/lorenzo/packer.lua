996bad8c333f291c8ee448c44efabc470c53a2ff
Lorenzo Martinez <33464406+Lor3nzoMartinez@users.noreply.github.com>
2025-01-16 22:52:05 -0800

    Update packer.lua

--- lua/lorenzo/packer.lua
+++ lua/lorenzo/packer.lua
@@ -59,4 +59,16 @@ return require('packer').startup(function(use)
     },
   }
 
+  use {
+   'dense-analysis',
+   config = function()
+     local g = vim.g
+
+     g.ale_ruby_rubocop_auto_correct_all = 1
+     g.ale_linters = {
+       ruby = {'rubocop', 'ruby'},
+       lua = {'lua_language_server'},
+       javascript = {'eslint'},
+      }
+    end
 end)
