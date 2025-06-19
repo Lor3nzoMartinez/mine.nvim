724272ea89abe9d3a0a6138da3ea640355c09554
Lorenzo <Lorenzo.s.martinez0@gmail.com>
2025-06-18 22:16:01 -0700

    Added: Precognition, Updated: NeoTree, Removed: dense-matter

--- lua/lorenzo/packer.lua
+++ lua/lorenzo/packer.lua
@@ -41,7 +41,16 @@ return require('packer').startup(function(use)
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = false, -- show hidden files
+          },
+        },
+      })
+    end
   }
 
   -- LSP Zero
@@ -59,18 +68,34 @@ return require('packer').startup(function(use)
     },
   }
 
-  -- Rubocop
   use {
-   'dense-analysis',
-   config = function()
-     local g = vim.g
-
-     g.ale_ruby_rubocop_auto_correct_all = 1
-     g.ale_linters = {
-       ruby = {'rubocop', 'ruby'},
-       lua = {'lua_language_server'},
-       javascript = {'eslint'},
-      }
-    end
+    "tris203/precognition.nvim",
+    -- event = "VeryLazy",
+    opts = {
+      startVisible = true,
+      showBlankVirtLine = true,
+      highlightColor = { link = "Comment" },
+      hints = {
+        Caret = { text = "^", prio = 2 },
+        Dollar = { text = "$", prio = 1 },
+        MatchingPair = { text = "%", prio = 5 },
+        Zero = { text = "0", prio = 1 },
+        w = { text = "w", prio = 10 },
+        b = { text = "b", prio = 9 },
+        e = { text = "e", prio = 8 },
+        W = { text = "W", prio = 7 },
+        B = { text = "B", prio = 6 },
+        E = { text = "E", prio = 5 },
+      },
+      gutterHints = {
+        G = { text = "G", prio = 10 },
+        gg = { text = "gg", prio = 9 },
+        PrevParagraph = { text = "{", prio = 8 },
+        NextParagraph = { text = "}", prio = 8 },
+      },
+      disabled_fts = {
+        "startify",
+      },
+    },
   }
 end)
