73c89a3e24cf745a319a4d64708e5176e5c75634
Lorenzo <Lorenzo.s.martinez0@gmail.com>
2025-06-24 20:21:38 -0700

    Switch neo-tree from float to left position

--- lua/lorenzo/remap.lua
+++ lua/lorenzo/remap.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>e", function()
   require("neo-tree.command").execute({
     toggle = true,
-    position = "float",
+    position = "left",
     dir = vim.fn.expand("%:p:h"),
   })
 end, { noremap = true, silent = true })
